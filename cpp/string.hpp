e160c9face6f5fa14f87da2a0decfa7889890508
Imanol Fotia <imanolfotia@gmail.com>
2024-11-13 23:59:41 -0300

    new constructor for string

--- cpp/string.hpp
+++ cpp/string.hpp
@@ -9,16 +9,18 @@ class JsString {
 public:
   JsString(const char *str) : ptr{(size_t)(str)}, length{pLength(str)} {}
   JsString(size_t str, size_t l) : ptr{str}, length{l} {}
+  JsString(size_t x) : ptr{x}{
+    length = pLength((char*)x);
+  }
 
-  size_t ptr;
-  size_t length;
+  size_t ptr{};
+  size_t length{};
 
 private:
   size_t pLength(const char *str) {
     char *s = (char *)str;
     int l = 0;
-    while (s++, *s)
-      l++;
+    while (*(s++)) l++;
     return l;
   }
 };
