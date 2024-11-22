dabcc796e278655e638e4e62a1b926253d89b945
Imanol Fotia <imanolfotia@gmail.com>
2024-11-21 23:59:53 -0300

    defined the rest of the fields in wgpu structs

--- cpp/webgpu.hpp
+++ cpp/webgpu.hpp
@@ -14,39 +14,190 @@ CREATE_HANDLE(Queue);
 CREATE_HANDLE(TextureView);
 CREATE_HANDLE(Texture);
 CREATE_HANDLE(Pipeline);
+CREATE_HANDLE(PipelineLayout);
 CREATE_HANDLE(RenderPass);
 CREATE_HANDLE(CommandEncoder);
 CREATE_HANDLE(CommandBuffer);
 CREATE_HANDLE(ShaderModule);
+CREATE_HANDLE(BindGroupLayout);
 CREATE_HANDLE(Buffer);
+CREATE_HANDLE(PipelineCache);
+CREATE_HANDLE(QuerySet);
 
 struct RenderTarget {
   size_t format;
 };
 
-struct PipelineLayout {
-
+struct VertexAttribute {
+  size_t format;
+  size_t offset;
+  size_t shaderLocation;
 };
 
-struct VertexStage {
-  
-}
+struct VertexBufferLayout {
+  size_t arrayStride;
+  size_t attributeCount;
+  VertexAttribute *attributes;
+};
 
-struct PipelineDescriptor {
-  PipelineLayout layout;
-  ShaderModule vertex;
-  ShaderModule fragment;
-  RenderTarget *targets;
+struct VertexState {
+  ShaderModule module;
+  JsString entryPoint;
+  size_t layoutsCount;
+  VertexBufferLayout *layouts;
+};
+
+struct BlendComponent {
+  size_t srcFactor;
+  size_t dstFactor;
+  size_t blendOperation;
+};
+
+
+struct BlendState {
+  BlendComponent color;
+  BlendComponent alpha;
+};
+
+struct ColorTargetState {
+  size_t format;
+  BlendState blend;
+  size_t writeMask;
+};
+
+
+struct FragmentState {
+  ShaderModule module;
+  JsString entryPoint;
   size_t targetCount;
-  size_t primitiveTopology;
-  size_t depthStencil[2];
+  ColorTargetState *targets;
+};
+
+struct BufferBindingLayout {
+  size_t type;
+};
+
+struct BindGroupLayoutEntry {
+  size_t binding;
+  size_t visibility;
+  size_t type;
+  size_t count;
+  // BufferBindingLayout buffer;
+};
+
+struct PushConstantRange {
+  size_t shaderStages;
+  size_t range[2];
+};
+
+struct BindGroupLayoutDescriptor {
+  ShaderModule module;
+  JsString entryPoint;
+  size_t entryCount;
+  BindGroupLayoutEntry *entries;
+};
+
+
+struct PipelineLayoutDescriptor {
+  size_t bindGroupLayoutCount;
+  BindGroupLayout *bindGroupLayouts;
+  size_t pushConstantRangesCount;
+  PushConstantRange *pushConstantRanges;
+};
+
+struct PrimitiveState {
+  size_t topology;
+  size_t stripIndexFormat;
+  size_t frontFace;
+  size_t cullMode;
+  bool unclippedDepth;
+  size_t polygonMode;
+  bool conservative;
+};
+
+struct StencilFaceState {
+  size_t compare;
+  size_t failOp;
+  size_t stencilFailOp;
+  size_t passOp;
+};
+
+struct StencilState {
+  StencilFaceState front;
+  StencilFaceState back;
+  size_t readMask;
+  size_t writeMask;
+};
+
+struct DepthBiasState {
+  size_t constant;
+  float slope_scale;
+  float clamp;
+};
+
+struct DepthStencilState {
+  size_t format;
+  bool depthWriteEnabled;
+  size_t depthCompare;
+  StencilState stencil;
+  DepthBiasState bias;
+};
+
+struct MultiSampleState {
+  size_t count;
+  size_t mask;
+  bool alphaToCoverageEnabled;
+};
+
+
+struct RenderPipelineDescriptor {
+  PipelineLayout layout;
+  VertexState vertex;
+  PrimitiveState primitive;
+  DepthStencilState depthStencil;
+  MultiSampleState multisample;
+  FragmentState fragment;
+  size_t miltiview;
+  PipelineCache cache;
+};
+
+struct Operations {
+  LoadOp load;
+  StoreOp store;
+};
+
+struct Color {
+  float v[4];
+};
+
+struct RenderPassColorAttachment {
+  TextureView view;
+  TextureView resolveView;
+  Operations operations;
+  Color clearValue;
+};
+
+struct RenderPassDepthStencilAttachment {
+  TextureView *view;
+  size_t depthOpsCount;
+  Operations depthOps;
+  size_t stencilOpsCount;
+  Operations stencilOps;
+};
+
+struct RenderPassTimestampWrites {
+  QuerySet *querySet;
+  size_t beginningOfPassWriteIndex;
+  size_t endOfPassWriteIndex;
 };
 
 struct RenderPassDescriptor {
-  TextureView textureView;
-  size_t clearValue[4];
-  size_t loadOp;
-  size_t storeOp;
+  size_t colorAttachmentsCount;
+  RenderPassColorAttachment *colorAttachments;
+  size_t depthStencilAttachmentsCount;
+  RenderPassDepthStencilAttachment *depthStencilAttachments;
+  RenderPassTimestampWrites timestampWrites;
+  QuerySet *occlusionQuerySet;
 };
 
 struct BufferDescriptor {
@@ -60,9 +211,10 @@ extern "C" {
 Device wgpuGetDevice();
 ShaderModule wgpuCreateShaderModule(Device device, JsString);
 size_t wgpuGetPreferredCanvasFormat();
-Pipeline wgpuCreateRenderPipeline(Device, PipelineDescriptor);
+Pipeline wgpuCreateRenderPipeline(Device, RenderPipelineDescriptor);
 CommandEncoder wgpuCreateCommandEncoder(Device);
 TextureView wgpuSwapChainGetCurrentTextureView();
+PipelineLayout wgpuCreatePipelineLayout(Device, PipelineLayoutDescriptor);
 RenderPass wgpuCommandEncoderBeginRenderPass(CommandEncoder,
                                              RenderPassDescriptor);
 void wgpuRenderPassEncoderSetPipeline(RenderPass, Pipeline);
@@ -92,7 +244,8 @@ static ShaderModule CreateShaderModule(Device device, JsString code) {
 static size_t GetPreferredCanvasFormat() {
   return imports::wgpuGetPreferredCanvasFormat();
 }
-static Pipeline CreateRenderPipeline(Device device, PipelineDescriptor info) {
+static Pipeline CreateRenderPipeline(Device device,
+                                     RenderPipelineDescriptor info) {
   return imports::wgpuCreateRenderPipeline(device, info);
 }
 
@@ -148,7 +301,6 @@ static Buffer CreateBuffer(Device device, BufferDescriptor commandBuffer) {
 }
 
 static void DestroyBuffer(Buffer buffer) { imports::wgpuDestroyBuffer(buffer); }
-
 
 static void RenderPassEncoderSetVertexBuffer(RenderPass encoder, size_t slot,
                                              Buffer buffer, size_t offset,
