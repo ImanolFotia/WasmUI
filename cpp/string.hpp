4e61d02a400fb6a3cc711db0a4caa91fd0927f1a
Imanol Fotia <imanolfotia@gmail.com>
2024-11-22 23:50:20 -0300

    moved implementations to cpp files

--- cpp/string.hpp
+++ cpp/string.hpp
@@ -2,28 +2,30 @@
 
 #include "defs.hpp"
 
+extern "C" {
+  [[maybe_unused]] void* memset(void* dest, int ch, size_t count);
+  [[maybe_unused]] void* memcpy(void* dest, void *src, size_t count);
+  [[maybe_unused]] size_t strlen(const char* str);
+}
 struct JsCString {
-  size_t p;
+  size_t data;
   size_t len;
 };
 
 class JsString {
 public:
   JsString() = default;
-  JsString(const char *str) : ptr{(size_t)(str)}, length{pLength(str)} {}
-  JsString(size_t str, size_t l) : ptr{str}, length{l} {}
-  JsString(size_t x) : ptr{x}{
-    length = pLength((char*)x);
+  JsString(const char *str) : data{(size_t)(str)}, length{strlen(str)} {}
+  JsString(size_t str, size_t l) : data{str}, length{l} {}
+  JsString(size_t x) : data{x}{
+    length = strlen((char*)x);
   }
 
-  size_t ptr{};
+  size_t data{};
   size_t length{};
 
 private:
-  size_t pLength(const char *str) {
-    char *s = (char *)str;
-    int l = 0;
-    while (*(s++)) l++;
-    return l;
-  }
 };
+
+
+extern "C" void puts(JsString);
