04a1c27ca13fb844ab0bd18dbdd5e144f2ff7731
Imanol Fotia <imanolfotia@gmail.com>
2025-02-04 20:51:31 -0300

    new dedicated arena

--- cpp/arena.hpp
+++ cpp/arena.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "defs.hpp"
+#include "drawable.hpp"
 
 extern "C" {
 extern unsigned char __heap_base;
+extern const uint32_t __heap_end;
 
 unsigned char *global_heap_position = &__heap_base;
 
@@ -35,12 +37,16 @@ extern "C" {
 #define ARENAS
 Arena transfer_arena;
 Arena io_arena;
+Arena primitive_vertex_arena;
+Arena primitive_index_arena;
 #endif
 
 typedef struct File_t *FILE;
 
 __attribute__((constructor)) static void arena_layout() {
-  create_arena(&transfer_arena, 64 * 1024 * 1024);
-  create_arena(&io_arena, sizeof(FILE) * 1024 + 8 * 1024 * 1024);
+  create_arena(&transfer_arena, 64 * 1024 * 1024); //64Mb
+  create_arena(&io_arena, 8 * 1024 * 1024); //8Mb
+  create_arena(&primitive_vertex_arena, sizeof(Math::Vertex) * 8192 * 2); //512Kb
+  create_arena(&primitive_index_arena, sizeof(uint32_t) * 8192 * 2); //64Kb
 }
 }
